139c083d386a8f3df938dcbfe589bb84522df77d
Sebastien Leger <sebastien.regel@gmail.com>
2021-10-12 20:55:33 +0200

    Update Bash install script

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -x
 #
 # Usage:
-#    install.sh [-u] [ -n NAMESPACE ] [ -r RELEASE ] [-k kes.skey] [-v vrf.skey] [-c node.cert]
+#    install.sh [-u] [ -n NAMESPACE ] [ -r RELEASE ] [-h hostName] [-v vaultName] [-i managedIdentityId] [-t tenantId]
 #
 set -eu
 
 usage() {
-  echo "$0 [-u] [ -n NAMESPACE ] [ -r RELEASE ] [-k kes.skey] [-v vrf.skey] [-c node.cert]"
+  echo "$0 [-u] [ -n NAMESPACE ] [ -r RELEASE ] [-h hostName] [-v vaultName] [-i managedIdentityId] [-t tenantId]"
 
   echo "Description of options:"
   echo "  -n: Set namespace. Default: testnet"
-  echo "  -r: Set release name. Default: testnet"
-  echo "  -k: KES skey file. Default: kes.skey"
-  echo "  -v: VRF skey file. Default: vrf.skey"
-  echo "  -c: Node certificate file. Default: node.cert"
+  echo "  -r: Set release name. Default equals to the namespace"
+  echo "  -h: DNS name label"
+  echo "  -v: Azure Vault name"
+  echo "  -i: Azure User Managed Identity identifier"
+  echo "  -t: Azure Tenant Id"
   echo "  -u: Uninstall"
 }
 
@@ -23,13 +24,14 @@ exit_abnormal() {
 }
 
 NAMESPACE=testnet
-RELEASE=testnet
+RELEASE=""
+HOSTNAME=stupidchess
 UNINSTALL=0
-KES_SKEY=""
-VRF_SKEY=""
-NODE_CERT=""
+TENANT_ID=""
+VAULT_NAME=""
+MANAGED_IDENTITY=""
 
-while getopts ":n:r:h:uc:k:v:" options; do
+while getopts ":n:r:h:uv:i:t:" options; do
   case "${options}" in
     u)
       UNINSTALL=1
@@ -40,14 +42,17 @@ while getopts ":n:r:h:uc:k:v:" options; do
     r)
       RELEASE=${OPTARG}
       ;;
-    c)
-      NODE_CERT=${OPTARG}
+    h)
+      HOSTNAME=${OPTARG}
       ;;
-    k)
-      KES_SKEY=${OPTARG}
+    i)
+      MANAGED_IDENTITY=${OPTARG}
       ;;
     v)
-      VRF_SKEY=${OPTARG}
+      VAULT_NAME=${OPTARG}
+      ;;
+    t)
+      TENANT_ID=${OPTARG}
       ;;
     :)
       echo "Error: -${OPTARG} requires an argument." >&2
@@ -58,43 +63,38 @@ while getopts ":n:r:h:uc:k:v:" options; do
       ;;
   esac
 done
-KES_SKEY=${KES_SKEY:-kes.skey}
-VRF_SKEY=${VRF_SKEY:-vrf.skey}
-NODE_CERT=${NODE_CERT:-node.cert}
+RELEASE=${RELEASE:-"$NAMESPACE"}
 
 if [ $UNINSTALL -ne 0 ]; then
   helm -n "$NAMESPACE" uninstall "$RELEASE"
   exit $?
 fi
 
-[[ -f "$KES_SKEY" ]] \
-  || { echo "File not found: $KES_SKEY" ; exit_abnormal; } >&2
-[[ -f "$VRF_SKEY" ]] \
-  || { echo "File not found: $VRF_SKEY" ; exit_abnormal; } >&2
-[[ -f "$NODE_CERT" ]] \
-  || { echo "File not found: $NODE_CERT" ; exit_abnormal; } >&2
+[[ -n "$HOSTNAME" ]] \
+  || { echo "Undefined DNS host name label" ; exit_abnormal; } >&2
+[[ -n "$TENANT_ID" ]] \
+  || { echo "Undefined tenant id" ; exit_abnormal; } >&2
+[[ -n "$VAULT_NAME" ]] \
+  || { echo "Undefined vault name" ; exit_abnormal; } >&2
+[[ -n "$MANAGED_IDENTITY" ]] \
+  || { echo "Undefined managed identity" ; exit_abnormal; } >&2
 
-KES_SKEY=$(realpath "$KES_SKEY")
-VRF_SKEY=$(realpath "$VRF_SKEY")
-NODE_CERT=$(realpath "$NODE_CERT")
 
 REDIS_USERNAME=${REDIS_USERNAME:-cardano}
 REDIS_PASSWORD=$(kubectl get secret --namespace "$NAMESPACE" "${RELEASE}-auth" -o jsonpath="{.data.redis-password}" | base64 -d)
 if [ -z "$REDIS_PASSWORD" ]; then
   REDIS_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n1)
 fi
-echo hello
 kubectl create namespace "$NAMESPACE" || true
 (
 cd cardano
 VALUES_FILE="values-${RELEASE}.yaml"
 if [ ! -f "$VALUES_FILE" ]; then
-  echo "Warn: Cannot find file ${VALUES_FILE}. Using defaults" >&2
-  VALUES_FILE="values.yaml"
+  { echo "Error: Cannot find file ${VALUES_FILE}" ; exit_abnormal; } >&2
 fi
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm dep update
-helm install prometheus bitnami/kube-prometheus -n prometheus
+helm install prometheus bitnami/kube-prometheus -n prometheus || true
 helm upgrade --install "$RELEASE" \
   --namespace $NAMESPACE \
   --values "$VALUES_FILE" \
@@ -102,12 +102,15 @@ helm upgrade --install "$RELEASE" \
   --set secrets.redisPassword="$REDIS_PASSWORD" \
   --set redis.auth.username="$REDIS_USERNAME" \
   --set redis.auth.password="$REDIS_PASSWORD" \
-  --set-file secrets.kesSkey=$KES_SKEY \
-  --set-file secrets.vrfSkey=$VRF_SKEY \
-  --set-file secrets.nodeCert=$NODE_CERT \
+  --set service.beta.kubernetes.io/azure-dns-label-name="$HOSTNAME" \
   --set metrics.enabled=true \
   --set metrics.serviceMonitor.enabled=true \
   --set metrics.serviceMonitor.namespace=prometheus \
+  --set vault.csi.enabled=true \
+  --set vault.csi.coldVaultName="$VAULT_NAME" \
+  --set vault.csi.hotVaultName="$VAULT_NAME" \
+  --set vault.csi.userAssignedIdentityID="$MANAGED_IDENTITY" \
+  --set vault.csi.tenantId="$TENANT_ID" \
   .
 )
 
